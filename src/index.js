137bb553ea677ff70635bca9587af95113e6569f
lijianhua <lijianhuawork@163.com>
2022-03-09 15:18:13 +0800

    fix：删除无用代码

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@
  * @Author: Joshua
  * @Date: 2022-02-28 10:58:32
  * @LastEditors: Joshua
- * @LastEditTime: 2022-03-05 18:34:12
+ * @LastEditTime: 2022-03-09 15:17:11
  */
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import reportWebVitals from './reportWebVitals'
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
@@ -21,4 +20,3 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
